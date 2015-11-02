158349b41874e47b4f343b9e838a0c3e81d97873
mwasyl <mwasyl@gmail.com>
2015-11-02 14:12:43 +0100

    Initial commit

---
+++ Kata.StringCalculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Kata.StringCalculator.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void Should_Calculate_EmptyString_ReturnsZero()
+        {
+            //Arrange
+
+            //Act
+            int result = StringCalculator.Calculate("");
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Should_Calculate_SingleNumber_ReturnsNumber()
+        {
+            //Arrange
+
+            //Act
+            int result = StringCalculator.Calculate("2");
+
+            //Assert
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Should_Calculate_TwoNumbersCommaDelimited_ReturnsSum()
+        {
+            //Arrange
+
+            //Act
+            int result = StringCalculator.Calculate("2,3");
+
+            //Assert
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Should_Calculate_TwoNumbersNewLineDelimited_ReturnsSum()
+        {
+            //Arrange
+
+            //Act
+            int result = StringCalculator.Calculate("2\n3");
+
+            //Assert
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Should_Calculate_ThreeNumbersEitherDelimited_ReturnsSum()
+        {
+            //Arrange
+
+            //Act
+            int result = StringCalculator.Calculate("2\n3,4");
+
+            //Assert
+            Assert.AreEqual(9, result);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Should_Calculate_NegativeNumbers_ThrowsException()
+        {
+            //Arrange
+
+            //Act
+            int result = StringCalculator.Calculate("-2\n3,4");
+
+            //Assert
+        }
+
+        [Test]
+        public void Should_Calculate_NumbersGreaterThan1000_AreIgnored()
+        {
+            //Arrange
+
+            //Act
+            int result = StringCalculator.Calculate("2\n1001,4");
+
+            //Assert
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void Should_Calculate_WithSingleLineDelimiter_ReturnsSum()
+        {
+            //Arrange
+
+            //Act
+            int result = StringCalculator.Calculate("//#2#3#4");
+
+            //Assert
+            Assert.AreEqual(9, result);
+        }
+
+        [Test]
+        public void Should_Calculate_WithMultiCharDelimiter_ReturnsSum()
+        {
+            //Arrange
+
+            //Act
+            int result = StringCalculator.Calculate("//[###]2###3###4");
+
+            //Assert
+            Assert.AreEqual(9, result);
+        }
+
+        [Test]
+        public void Should_Calculate_WithManySingleOrMultiCharDelimiter_ReturnsSum()
+        {
+            //Arrange
+
+            //Act
+            int result = StringCalculator.Calculate("//[$][,]2$3,4,5");
+
+            //Assert
+            Assert.AreEqual(14, result);
+        }  
+    }
+}
---
+++ Kata.StringCalculator/StringCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.StringCalculator
+{
+    public class StringCalculator
+    {
+        public static int Calculate(string numbers)
+        {
+            if (numbers == string.Empty) return 0;
+            var numbersFirstChar = 0;
+            var delimiters = new List<string>();
+
+            if (numbers.StartsWith("//["))
+            {
+                var delimiterEndTag = 0;
+                while (numbers.IndexOf('[', delimiterEndTag) > 0)
+                {
+                    var delimiterStartTag = numbers.IndexOf('[', delimiterEndTag) + 1;
+                    delimiterEndTag = numbers.IndexOf(']', delimiterStartTag);
+                    delimiters.Add(numbers.Substring(delimiterStartTag, delimiterEndTag - delimiterStartTag));
+                    numbersFirstChar = delimiterEndTag + 1;
+                }
+            }
+            else if (numbers.StartsWith("//"))
+            {
+                numbersFirstChar = 3;
+                delimiters.Add(numbers.Substring(2, 1));   
+            }
+            else
+            {
+                delimiters.Add(",");
+                delimiters.Add("\n");
+            }
+
+            numbers = numbers.Substring(numbersFirstChar, numbers.Length - numbersFirstChar);
+            var numbersArray = numbers.Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            if(numbersArray.Any(x => int.Parse(x) < 0)) throw new ArgumentException();
+            var sum = numbersArray.Sum(s => int.Parse(s) > 1000 ? 0: int.Parse(s));            
+            return sum;
+        }
+    }
+}
